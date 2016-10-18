72a5ecbd5e53f48105b64ba2bb30e8fe2c21f9ae
Sandeep J <jagata.sandeep@gmail.com>
2016-10-18 13:20:04 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ var myApp = angular.module('myApp', [
 ]);
 
 myApp.config(['$routeProvider', function ($routeProvider) {
+    console.log("app config");
+    
     $routeProvider.
     when('/list', {
         templateUrl: 'partials/list.html',
@@ -21,3 +23,32 @@ myApp.config(['$routeProvider', function ($routeProvider) {
         redirectTo: '/list'
     });
 }]);
+
+
+
+
+myApp.factory('aProvider', function() {
+   console.log("factory");
+});
+
+myApp.directive("testdirective", function() {
+    console.log("directive setup");
+    return {
+        compile: function() {console.log("testdirective compile");}
+    }
+});
+
+myApp.directive("testdirective2", function() {
+    return {
+        link: function() {console.log("testdirective2 link");}
+    }
+});
+
+myApp.run(function() {
+    console.log("app run");
+});
+
+myApp.controller('myCtrl', function($scope) {
+    console.log("app controller");
+});
+
